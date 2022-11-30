508b10d0e5c7044f180be2e9940ea344884f9d7b
antoniluthfi <luthfiantoni027@gmail.com>
2022-11-30 12:02:36 +0800

    change icon with image

--- src/components/CustomCarousel/CarouselButton.tsx
+++ src/components/CustomCarousel/CarouselButton.tsx
@@ -1,6 +1,6 @@
+import {ic_rounded_arrow_left, ic_rounded_arrow_right} from 'assets/icons';
 import React from 'react';
-import {StyleSheet, TouchableOpacity} from 'react-native';
-import Icon from 'react-native-vector-icons/AntDesign';
+import {Image, StyleSheet, TouchableOpacity} from 'react-native';
 
 interface IProps {
   iconName: 'arrowright' | 'arrowleft';
@@ -15,7 +15,14 @@ const CarouselButton: React.FC<IProps> = ({iconName, onPress}) => {
         iconName === 'arrowleft' ? {left: 20} : {right: 20},
       ]}
       onPress={onPress}>
-      <Icon name={iconName} size={25} color="white" />
+      <Image
+        source={
+          iconName === 'arrowleft'
+            ? ic_rounded_arrow_left
+            : ic_rounded_arrow_right
+        }
+        style={styles.arrowImage}
+      />
     </TouchableOpacity>
   );
 };
@@ -24,13 +31,14 @@ export default CarouselButton;
 
 const styles = StyleSheet.create({
   button: {
-    width: 30,
-    height: 30,
+    width: 31,
+    height: 31,
     borderRadius: 50,
-    backgroundColor: '#BBBBBB',
+    backgroundColor: 'white',
     justifyContent: 'center',
     alignItems: 'center',
     position: 'absolute',
     bottom: '45%',
   },
+  arrowImage: {width: 30, height: 30},
 });
